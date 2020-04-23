a3c1aa7e67aa2c8969ba2e5b0e7ed0cc86e4b3cc
jmoraal <jmoraal@users.noreply.github.com>
2020-04-23 21:47:47 +0200

    Update trivial_sigma_algebra.v

--- trivial_sigma_algebra.v
+++ trivial_sigma_algebra.v
@@ -1,15 +1,15 @@
 (*Version 1.5.6 - 23-04-2020
-  Lemma's for pi&lambda => sigma.  
+  Lemma's for pi&lambda => sigma moved to other file. 
+  U as universe, so that Om = full set (already in previous versions)
 *)
 Require Import Sets.Ensembles.
 Require Import Sets.Classical_sets.
 Require Import wplib.Tactics.Tactics.
 Require Import wplib.Tactics.TacticsContra.
-(*Require Import wplib.Notations.Notations.*)
 Require Import Sets.Powerset.
 Require Import Coq.Logic.Classical_Pred_Type. 
 
-Variable U : Type.
+Variable U : Type. 
 
 Notation "∅" := 
   (Empty_set U). 
@@ -48,7 +48,6 @@ Definition is_π_system (Π : Ensemble (Ensemble U))
     ∀ A : Ensemble U, A ∈ Π ⇒ 
       ∀ B : Ensemble U, B ∈ Π ⇒ 
          (A ∩ B) ∈ Π. 
-       (*In (Ensemble U) Π (Intersection _ A B). *)
 
 Definition Countable_union (A : (ℕ → Ensemble U)) 
   : Ensemble U := 
@@ -89,7 +88,7 @@ Definition is_σ_algebra (F : Ensemble (Ensemble U))
 Definition  σ_algebra_generated_by (A : Ensemble (Ensemble U)) 
   : (Ensemble (Ensemble U)) := 
     fun (B : Ensemble U) ↦ 
-    (∀ F : Ensemble (Ensemble U), (is_σ_algebra F ∧ A ⊂ F) ⇒ B ∈ F). 
+      (∀ F : Ensemble (Ensemble U), (is_σ_algebra F ∧ A ⊂ F) ⇒ B ∈ F). 
 (*
 Definition restriction (F : Ensemble (Ensemble U)) (A : (Ensemble U)) 
   : (Ensemble (Ensemble U)) := 
@@ -109,7 +108,6 @@ Take x : U; Assume x_in_full.
 It holds that (x ∈ (Ω \ ∅)).
 
 Take x : U; Assume x_in_complement_empty.
-(*Because x_in_complement_empty both x_in_full and not_x_in_empty. *)
 It holds that (x ∈ Ω). 
 Qed. 
 
@@ -143,8 +141,8 @@ split.
 Expand the definition of complement_in_set. 
 Take A : (Ensemble U). 
 Assume A_in_F : (A ∈ empty_and_full). 
-Write A_in_F as ( (A = Ω) 
-  ∨ (A = ∅) ).
+Write A_in_F as 
+  ((A = Ω) ∨ (A = ∅) ).
 Because A_in_F either A_is_full or A_is_empty. 
 rewrite -> A_is_full. 
 (*does the same as: 
@@ -178,11 +176,10 @@ Because all_or_not_all_empty
 It suffices to show that (Countable_union C = ∅). 
 We prove equality by proving two inclusions. 
 
-Expand the definition of Countable_union. 
 Take x : U; Assume x_in_countable_union_C. 
 Choose n such that x_in_C_n according to x_in_countable_union_C. 
-Write x_in_C_n using (C n = ∅) as (∅ x).
-It holds that (x ∈ ∅). 
+Write x_in_C_n using (C n = ∅) as (x ∈ ∅).
+Contradiction. 
 
 Take x : U; Assume x_in_empty. 
 Contradiction. 
@@ -213,127 +210,3 @@ Choose n1 such that C_n1_full according to one_full.
 rewrite <- C_n1_full. 
 It holds that ((C n1) ⊂ (Countable_union C)). 
 Qed. 
-
-Inductive auxiliary_seq (C : (ℕ ⇨ Ensemble U)) 
-  : (ℕ ⇨ Ensemble U) := 
-    | aux_first : (auxiliary_seq C) 0 = ∅ 
-    | aux_ind : ∀ n : ℕ, 
-        (auxiliary_seq C) n 
-        = (C n) ∪ ((auxiliary_seq C) (n-1)). 
-
-(* Waarom werkt deze niet? Vergelijk met deze: 
-   Inductive Couple (x y:U) : Ensemble :=
-    | Couple_l : In (Couple x y) x
-    | Couple_r : In (Couple x y) y.
-*)
-
-Inductive make_disjoint_seq_sets (C : (ℕ ⇨ Ensemble U)) 
-  : (ℕ ⇨ Ensemble U) := 
-    | first_set : (make_disjoint_seq_sets C 1 = C 0) 
-    | induction_sets : ∀ n : ℕ, make_disjoint_seq_sets C n = (C n) \ (auxiliary_seq C n). 
-
-Lemma complement_as_intersection : 
-  ∀ A B : Ensemble U, 
-    A \ B = A ∩ (Ω \ B). 
-
-Proof. 
-Take A : (Ensemble U); Take B : (Ensemble U). 
-We prove equality by proving two inclusions. 
-
-Take x : U. 
-It holds that (In _ (Full_set _) x) (x_in_full). (*Waarom werkt dit niet?*)
-Assume x_in_A_without_B. 
-It holds that (~(x ∈ B)) (x_not_in_B).
-
-x ∈ Full_set U) (x_in_Omega). 
-By x_not_in_B it holds that (x ∈ (Ω \ B)) (x_in_complement_B). 
-
-Lemma complements_in_pi_lambda_system : 
-  ∀ F : Ensemble (Ensemble U), 
-    is_π_system F ∧ is_λ_system F 
-    ⇒ ∀ A B : Ensemble U, A ∈ F ∧ B ∈ F
-      ⇒ A \ B ∈ F. 
-
-Proof. 
-Take F : (Ensemble (Ensemble U)). 
-Assume F_is_π_and_λ_system.
-By F_is_π_and_λ_system 
-  it holds that (is_π_system F) (F_is_π_system). 
-By F_is_π_and_λ_system 
-  it holds that (is_λ_system F) (F_is_λ_system). 
-Take A : (Ensemble U); Take B : (Ensemble U). 
-Assume A_and_B_in_F. 
-
-
-
-
-Lemma π_and_λ_is_σ : 
-  ∀ F : Ensemble (Ensemble U), 
-    is_π_system F ∧ is_λ_system F 
-    ⇒ is_σ_algebra F. 
-
-Proof. 
-Take F : (Ensemble (Ensemble U)).
-Assume F_is_π_and_λ_system. 
-By F_is_π_and_λ_system 
-  it holds that (is_π_system F) (F_is_π_system). 
-By F_is_π_and_λ_system 
-  it holds that (is_λ_system F) (F_is_λ_system). 
-Expand the definition of is_σ_algebra.
-split.
-It holds that (full_set_in_set F) .
-split.
-It holds that (complement_in_set F). 
-
-Expand the definition of closed_under_countable_union. 
-Take C : (ℕ ⇨ Ensemble U); Assume all_C_n_in_F. 
-(*It holds that (closed_under_disjoint_countable_union F) (F_closed_under_disjoint). 
-Expand the definition of 
-  closed_under_disjoint_countable_union 
-    in F_closed_under_disjoint. *)
-By classic it holds that 
-  ((∀ m n : ℕ, m ≠ n ⇒ Disjoint _ (C m) (C n)) ∨ 
-  ¬(∀ m n : ℕ, m ≠ n ⇒ Disjoint _ (C m) (C n))) (all_or_not_all_disjoint). 
-Because all_or_not_all_disjoint either all_disjoint or not_all_disjoint. 
-(*Case 1: all C_n disjoint.*) 
-It holds that (Countable_union C ∈ F). 
-
-(*Case 2: not all C_n disjoint. *)
-(*Newer approach: *)
-
-(*Former approach: *)
-By not_all_disjoint it holds that 
-  (∃m n : ℕ, m ≠ n 
-    ⇨ ¬(Disjoint U (C m) (C n))) (two_not_disjoint). 
-Choose m 
-  such that one_not_disjoint_with_m 
-    according to two_not_disjoint. 
-Choose n 
-  such that C_m_n_not_disjoint 
-    according to one_not_disjoint_with_m.
-(*Nu nog m ≠ n? *)
-It holds that ((C m) ∈ F ∧ (C n) ∈ F) (C_m_and_C_m_in_F). 
-By F_is_π_system it holds that 
-  ((Intersection _ (C m) (C n)) ∈ F) (intersection_in_F).
-(*Dead end? *)
-
-(* Usual proof method: 
-   Let B_n := C_n \ (union i=1 to n-1 of C_i). 
-    (or: A_n := A_n-1 ∪ B_n-1, A_0 = empty
-         B_n := C_n \ A_n, B_0 = C_0)
-   These are in F by F_is_π_system, and their union
-   is in F by F_is_λ_system
-*)
-
- 
-(*Qed. *) Admitted. 
-
-Theorem π_λ_theorem : 
-  ∀ Π Λ : Ensemble (Ensemble U), 
-    is_π_system Π ∧ is_λ_system Λ ∧ Π ⊂ Λ
-    ⇒ (σ_algebra_generated_by Π) ⊂ Λ. 
-
-
-
-
-
